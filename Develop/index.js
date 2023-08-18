c77f8a31df469af5fcff24fea0adce864d92031e
oSweets13 <olivia13perkins@gmail.com>
2023-08-18 09:54:54 -0600

    added my functions and installed the packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,23 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const { default: inquirer } = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if(err) throw new Error(err);
+        console.log('ReadMe Successfully Generated!')
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
